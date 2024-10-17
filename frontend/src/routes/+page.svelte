cefd45c3f45802f93ba97a7fe0c38b3890649723
Rubén J <rubenjr@protonmail.com>
2024-10-17 15:34:41 +0200

    bulma

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -29,5 +29,12 @@
 <h3 class="text-xl">Por si acaso</h3> -->
 
 {#each cards as card}
-    <Card name={card.name} distance={card.distance} />
+    <Card />
 {/each}
+
+<div class="buttons">
+    <button class="button is-info">Info</button>
+    <button class="button is-success">Success</button>
+    <button class="button is-warning">Warning</button>
+    <button class="button is-danger">Danger</button>
+</div>
