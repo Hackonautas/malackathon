9875a8042c0e1dfa8006362324d452983eff12c7
Rubén J <rubenjr@protonmail.com>
2024-10-17 14:57:50 +0200

    removed db stuff

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,9 @@
-use axum::{routing::get, Router};
-use routes::{all_reservoirs, reservoir_by_id, reservoir_history};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use routes::{all_reservoirs, reservoir_by_id, reservoir_history, reservoirs_in_range};
+use tracing::info;
 
 mod models;
 mod routes;
@@ -9,14 +13,23 @@ mod services;
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    services::connect_to_db().await;
+
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         .route("/reservoirs", get(all_reservoirs))
+        .route("/reservoirs", post(reservoirs_in_range))
         .route("/reservoir/:id", get(reservoir_by_id))
         .route("/reservoir/:id/historical", get(reservoir_history));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+
+    info!("Listening on port {}", port);
 
     axum::serve(listener, app).await.unwrap();
 }
