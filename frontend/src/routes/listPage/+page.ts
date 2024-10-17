57d194c8196c1afacac15c81e41eda6b8abed569
Rubén J <rubenjr@protonmail.com>
2024-10-17 18:48:58 +0200

    minimal connection to backend

--- frontend/src/routes/listPage/+page.ts
+++ frontend/src/routes/listPage/+page.ts
@@ -1,3 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+export async function load() {
+  const res = await fetch("http://localhost:3000/reservoirs");
+  const embalses = await res.json();
+  console.log(embalses);
+  return {
+    embalses: embalses,
+  };
+}
