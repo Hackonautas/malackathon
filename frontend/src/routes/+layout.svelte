4949da646087a4036038eb1b625375ad5a0e24a8
JOSÉ MARÍA NOGUERAS NAVARRO <jomanone2@gmail.com>
2024-10-17 11:44:30 +0200

    1.0
    Protopágina

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,53 +1,53 @@
 <script>
 	import Header from './Header.svelte';
 	import '../app.css';
-</script>
-
-<div class="app">
+  </script>
+  
+  <div class="app bg-gray-900 text-white">
 	<Header />
-
-	<main>
-		<slot />
+  
+	<main class="p-6">
+	  <slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  
+	<footer class="bg-blue-900 p-4 text-center">
+	  <p>Visit <a href="https://kit.svelte.dev" class="text-blue-400">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
-</div>
-
-<style>
+  </div>
+  
+  <style>
 	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
+	  display: flex;
+	  flex-direction: column;
+	  min-height: 100vh;
 	}
-
+  
 	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
+	  flex: 1;
+	  display: flex;
+	  flex-direction: column;
+	  padding: 1rem;
+	  width: 100%;
+	  max-width: 64rem;
+	  margin: 0 auto;
+	  box-sizing: border-box;
 	}
-
+  
 	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	  align-items: center;
+	  padding: 12px;
 	}
-
+  
 	footer a {
-		font-weight: bold;
+	  font-weight: bold;
 	}
-
+  
 	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+	  footer {
+		padding: 12px 0;
+	  }
 	}
-</style>
+  </style>
