61cd4873ee55bf9b1e4c69c3984b87e01df84de6
Rubén J <rubenjr@protonmail.com>
2024-10-17 12:09:07 +0200

    basic routes and models

---
+++ backend/src/models.rs
@@ -0,0 +1,20 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct Coords {
+    lat: f64,
+    lon: f64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Reservoir {
+    id: usize,
+    code: usize,
+    name: String,
+    common_name: String,
+    current: f32,
+    capacity: f32,
+    location: Coords,
+    province: String,
+    ccaa: String,
+}
