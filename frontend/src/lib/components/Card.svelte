abd71a8993dd5f7cccfce543219b21b7cc6afe9c
Rubén J <rubenjr@protonmail.com>
2024-10-17 13:04:21 +0200

    basic card

---
+++ frontend/src/lib/components/Card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let name: string;
+    export let distance: number;
+</script>
+
+<div class="card bg-base-100 w-96 shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">{name}</h2>
+        <p>{distance} km</p>
+    </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script>
+    import Card from "$lib/components/Card.svelte";
+
+    let cards = [
+        {
+            name: "patata",
+            distance: 30,
+        },
+        {
+            name: "tomate",
+            distance: 40,
+        },
+        {
+            name: "cebolla",
+            distance: 50,
+        },
+    ];
+</script>
+
 <!-- <svelte:head>
     <title>Water Management</title>
     <meta
@@ -8,3 +27,7 @@
 
 <!-- <h1 class="text-3xl">Tus muertos</h1>
 <h3 class="text-xl">Por si acaso</h3> -->
+
+{#each cards as card}
+    <Card name={card.name} distance={card.distance} />
+{/each}
