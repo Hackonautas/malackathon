6524b268841d46eced86799a3cab32f4c52a90f8
Rubén J <rubenjr@protonmail.com>
2024-10-17 14:57:50 +0200

    removed db stuff

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,15 +1,25 @@
 use axum::Json;
+use tracing::info;
 
-use crate::models::Reservoir;
+use crate::models::{Coords, Reservoir};
 
+/// Get all reservoirs
 pub async fn all_reservoirs() -> Json<Vec<Reservoir>> {
     Json(vec![])
 }
 
+/// Get reservoirs in range
+pub async fn reservoirs_in_range(Json(coordinates): Json<Coords>) -> Json<Vec<Reservoir>> {
+    info!("Received coordinates: {:?}", coordinates);
+    Json(vec![])
+}
+
+/// Get reservoir by ID
 pub async fn reservoir_by_id() -> Json<Option<Reservoir>> {
     Json(None)
 }
 
+/// Get reservoir history
 pub async fn reservoir_history() -> Json<Vec<f32>> {
     Json(vec![])
 }
