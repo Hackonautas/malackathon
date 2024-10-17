893724982320f21b73e14bef67facdc4125d3a70
Rubén J <rubenjr@protonmail.com>
2024-10-17 11:26:39 +0200

    backend init

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    info!("Hello, world!");
+
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> String {
+    "hello".to_string()
 }
