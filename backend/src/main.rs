30e3653e8b1252b6989c066f71f26ffc0a900fce
Rubén J <rubenjr@protonmail.com>
2024-10-17 12:09:07 +0200

    basic routes and models

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,23 @@
 use axum::{routing::get, Router};
-use tracing::info;
+use routes::{all_reservoirs, reservoir_by_id, reservoir_history};
+
+mod models;
+mod routes;
+mod services;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    info!("Hello, world!");
-
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/reservoirs", get(all_reservoirs))
+        .route("/reservoir/:id", get(reservoir_by_id))
+        .route("/reservoir/:id/historical", get(reservoir_history));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
 
